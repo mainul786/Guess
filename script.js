166400ac64a183390abfe14680a7451563750ed0
mainul786 <mainulislam320@gmail.com>
2022-08-02 12:03:26 +0530

    guesing a number

---
+++ script.js
@@ -0,0 +1,61 @@
+let secretNumber = Math.trunc(Math.random() * 20 ) +1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message){
+  document.querySelector('.message').textContent =message;
+}
+
+document.querySelector('.check').addEventListener('click', function(){
+ const guess =Number(document.querySelector('.guess').value);
+if(!guess){
+  // document.querySelector('.message').textContent= '😂 No Number';
+  displayMessage('😂 No Number');
+} else if (guess === secretNumber){
+  // document.querySelector('.message').textContent ='currect Number';
+  displayMessage('currect Number');
+  document.querySelector('.number').textContent = secretNumber;
+  document.querySelector('body').style.backgroundColor='#60b347';
+  document.querySelector('.number').style.width='30rem';  
+  if(score > highScore){
+    highScore = score;
+    document.querySelector('.highscore').textContent = highScore;
+  }
+}else if( guess > secretNumber){
+ if(score > 1){
+  // document.querySelector('.message').textContent= '🎢 Too High!';
+  displayMessage('🎢 Too High!');
+  score--;
+  document.querySelector('.score').textContent= score;
+ }else{
+  // document.querySelector('.message').textContent = '🎉 You lost the Game';
+  displayMessage('🎉 You lost the Game!');
+  document.querySelector('.score').textContent= 0;
+ }
+
+  
+}else if(guess < secretNumber){
+  if(score > 1){
+    // document.querySelector('.message').textContent= '🎢 Too Low!';
+    displayMessage('🎢 Too Low!');
+    score--;
+    document.querySelector('.score').textContent= score;
+   }else{
+    // document.querySelector('.message').textContent = '🎉 You lost the Game';
+    displayMessage('🎉 You lost the Game!')
+    document.querySelector('.score').textContent= 0;
+   }
+ 
+}
+});
+document.querySelector('.again').addEventListener('click', function(){
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  // document.querySelector('.message').textContent='Start Gussing...';
+  displayMessage('Start guessing....')
+  document.querySelector('.score').textContent=score;
+  document.querySelector('.number').textContent='?';
+  document.querySelector('.guess').value= '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width='15rem';
+})
